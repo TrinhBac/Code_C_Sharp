390f602b60a4b9d265d08e3c531118c148a50679
Trinh Bac <trinhbac1907@gmail.com>
2020-10-14 20:12:40 +0700

    code lại bài kiểm tra class - hướng đối tượng 14/10/2020

---
+++ bai kiem tra OOP 14.10.2020/lop3/HangHoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lop3
+{
+    class HangHoa
+    {
+        string _ten;
+        double _soluong;
+        double _gia;
+
+        public string Ten { get => _ten; set => _ten = value; }
+        public double Soluong { get => _soluong; set => _soluong = value; }
+        public double Gia { get => _gia; set => _gia = value; }
+
+        public HangHoa() { }
+
+        public HangHoa(string tenHang, double soLuong, double giaTien)
+        {
+            this._ten = tenHang;
+            this._soluong = soLuong;
+            this._gia = giaTien;
+        }
+
+        public double TongTien()
+        {
+            return this._soluong * this._gia;
+        }
+
+        public override string ToString()
+        {
+            string st = $"{this._ten,15}{this._soluong,15}{this._gia,15}{this.TongTien(),15}";
+            return string.Format(st);
+        }
+    }
+}
---
+++ bai kiem tra OOP 14.10.2020/lop3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lop3
+{
+    class Program
+    {
+        private static List<HangHoa> a = new List<HangHoa>();
+
+        private static HangHoa AddProduct()
+        {
+            Console.Write("Nhap ten hang: ");
+            string ten = Console.ReadLine();
+            Console.Write("Nhap so luong: ");
+            double soluong = double.Parse(Console.ReadLine());
+            Console.Write("Nhap gia: ");
+            double gia = double.Parse(Console.ReadLine());
+            HangHoa tam = new HangHoa(ten, soluong, gia);
+            return tam;
+        }
+        private static void DisplayAll()
+        {
+            Console.WriteLine($"{"TEN HANG",15}{"SO LUONG",15}{"GIA",15}{"TONG TIEN",15}");
+            for (int i = 0; i < a.Count; i++)
+                Console.WriteLine(a[i].ToString());
+        }
+
+        private static bool EditProduct()
+        {
+            Console.Write("Nhap ten hang can sua: ");
+            string ten = Console.ReadLine();
+            for (int i = 0; i < a.Count; i++)
+            {
+                if(a[i].Ten == ten)
+                {
+                    HangHoa tam = new HangHoa();
+                    tam = AddProduct();
+                    a[i] = tam;
+                    Console.WriteLine($"Da cap nhat hang hoa co ten {ten}");
+                    return true;
+                }
+            }
+            Console.WriteLine($"Khong ton tai hang hoa co ten {ten}");
+            return false;
+        }
+
+        private static bool DeleteProduct()
+        {
+            Console.Write("Nhap ten hang can xoa: ");
+            string ten = Console.ReadLine();
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i].Ten == ten)
+                {
+                    a.Remove(a[i]);
+                    Console.WriteLine($"Da xoa hang hoa co ten {ten}");
+                    return true;
+                }
+            }
+            Console.WriteLine($"Khong ton tai hang hoa co ten {ten}");
+            return false;
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Nhap thong tin hang");
+            Console.WriteLine("2. Hien thi danh sach");
+            Console.WriteLine("3. Cap nhat thong tin hang");
+            Console.WriteLine("4. Xoa mot mat hang");
+            Console.WriteLine("5. Thoat");
+
+            while(true)
+            {
+                Console.Write("\nNhap lua chon: ");
+                string chon = Console.ReadLine();
+                switch (chon)
+                {
+                    case "1":
+                        a.Add(AddProduct());
+                        break;
+                    case "2":
+                        DisplayAll();
+                        break;
+                    case "3":
+                        EditProduct();
+                        break;
+                    case "4":
+                        DeleteProduct();
+                        break;
+                    case "5":
+                        return 0;
+                    default:
+                        Console.WriteLine("Khong hop le");
+                        break;
+                }
+            }
+        }
+    }
+}
