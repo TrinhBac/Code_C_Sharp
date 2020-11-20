5c10bfe4bf1c71f0ed7d78c267b17aa307ac9c55
Trinh Bac <trinhbac1907@gmail.com>
2020-11-20 21:38:11 +0700

    Giải đề thi .net, bài số 2, thi ngày 18/11/2020 10đ

---
+++ De_Ktra_Lan_2 ngay 18.11.2020/De_Ktra_Lan_2/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace De_Ktra_Lan_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quan ly sach";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã sách :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên sách :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(167, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số trang :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Năm XB :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(293, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(329, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(293, 175);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(329, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(293, 221);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(329, 22);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(85, 337);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 150);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(785, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(785, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(785, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 28);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Xoá";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(785, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 28);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(170, 279);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 21);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Sách điện tử";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaSach";
+            this.Column1.HeaderText = "Mã sách";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "TenSach";
+            this.Column2.HeaderText = "Tên sách";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "SoTrang";
+            this.Column3.HeaderText = "Số trang";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "NamXB";
+            this.Column4.HeaderText = "Năm XB";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 534);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ De_Ktra_Lan_2 ngay 18.11.2020/De_Ktra_Lan_2/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace De_Ktra_Lan_2
+{
+    public partial class Form1 : Form
+    {
+        string connStr = @"Data Source=DESKTOP-3IL1R52\SQLEXPRESS;Initial Catalog=Ktra_Lan_2;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)  //thoat
+        {
+            Application.Exit();
+        }
+
+        private void FillDataGridView()
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            string sqlStr = $"SELECT * FROM Sach";
+            SqlCommand com = new SqlCommand(sqlStr, conn);
+            com.CommandType = CommandType.Text;
+
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da.Dispose();
+            conn.Close();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillDataGridView();
+            textBox1.Focus();
+        }
+
+        private bool CheckMaSP(string masp)
+        {
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            string sqlStr = $"SELECT * FROM Sach WHERE MaSach = '{masp}'";
+            SqlCommand com = new SqlCommand(sqlStr, conn);
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            da.Dispose();
+            conn.Close();
+            if (dt.Rows.Count == 0) return true;    //chưa tồn tại mã
+            else return false;  // đã tồn tại mã
+        }
+
+        private void button1_Click(object sender, EventArgs e)  //them
+        {
+            if (String.IsNullOrWhiteSpace(textBox1.Text) || String.IsNullOrWhiteSpace(textBox2.Text)
+                || String.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Thiếu thông tin", "Thông báo");
+            }
+            else
+            {
+                if (CheckMaSP(textBox1.Text) == true)
+                {
+                    SqlConnection conn = new SqlConnection(connStr);
+                    conn.Open();
+                    bool sachDT = false;
+                    if (checkBox1.Checked == true)
+                        sachDT = true;
+                    string sqlStr = $"INSERT INTO Sach VALUES('{textBox1.Text}'," +
+                        $"N'{textBox2.Text}', '{int.Parse(textBox3.Text)}', '{dateTimePicker1.Value}', '{sachDT}')";
+                    SqlCommand com = new SqlCommand(sqlStr, conn);
+                    com.ExecuteNonQuery();
+                    conn.Close();
+                    FillDataGridView();
+                }
+                else
+                    MessageBox.Show("Đã tồn tại mã sách", "Thông báo");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)   //check dữ liệu số trang
+        {
+            if (!String.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                try
+                {
+                    int.Parse(textBox3.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Dữ liệu phải là số", "Thông báo");
+                    textBox3.Clear();
+                    textBox3.Focus();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //xoa
+        {
+            if (CheckMaSP(textBox1.Text) == false)
+            {
+                SqlConnection conn = new SqlConnection(connStr);
+                conn.Open();
+                string sqlStr = $"DELETE FROM Sach WHERE MaSach = '{textBox1.Text}'";
+                SqlCommand com = new SqlCommand(sqlStr, conn);
+                com.ExecuteNonQuery();
+                conn.Close();
+                FillDataGridView();
+            }
+            else
+                MessageBox.Show("Khong ton tai sach", "Thong bao");
+        }
+
+        private void button2_Click(object sender, EventArgs e)   //Sua
+        {
+            if (CheckMaSP(textBox1.Text) == false)
+            {
+                SqlConnection conn = new SqlConnection(connStr);
+                conn.Open();
+                bool sachDT = false;
+                if (checkBox1.Checked == true)
+                    sachDT = true;
+                string sqlStr = $"UPDATE Sach SET TenSach = N'{textBox2.Text}', " +
+                    $"SoTrang = '{int.Parse(textBox3.Text)}', NamXB = '{dateTimePicker1.Value}', " +
+                    $"SachDT = '{sachDT}' WHERE MaSach = '{textBox1.Text}'";
+
+                SqlCommand com = new SqlCommand(sqlStr, conn);
+                com.ExecuteNonQuery();
+                conn.Close();
+                FillDataGridView();
+            }
+            else
+                MessageBox.Show("Không tồn tại bản ghi", "Thông báo");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)    //cell click
+        {
+            int i = e.RowIndex;
+            if (i > -1)
+            {
+                textBox1.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                textBox2.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                textBox3.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                dateTimePicker1.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+                if (dataGridView1.Rows[i].Cells[4].Value.ToString() == "True")
+                    checkBox1.Checked = true;
+                else checkBox1.Checked = false;
+            }
+        }
+    }
+}
